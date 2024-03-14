9e9a30e464c7f2aa64f0671726bd927f6bb39289
dereklopez <dereklopezgarcia@gmai.com>
2024-03-14 18:24:42 +0100

    Ejercici2-A

---
+++ src/main/java/com/iesam/ryanair/Test/Vuelo/ComprobarDisponibilidadAvionUseCaseTest.java
@@ -0,0 +1,24 @@
+package com.iesam.ryanair.Test.Vuelo;
+
+import com.iesam.ryanair.features.vuelo.data.local.VueloFileLocalDataSource;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ComprobarDisponibilidadAvionUseCaseTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+    @Test
+    void execute() {
+        ComprobarDisponibilidadAvionUseCaseTest comprobarDisponibilidadAvionUseCaseTest = new ComprobarDisponibilidadAvionUseCaseTest();
+        assertNotEquals();
+    }
+}
